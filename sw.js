c0c4d10accb946bf596ab903789f143edeb5dd40
marctaljard <marctaljard@outlook.com>
2026-07-29 16:48:51 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'cutprog-v2';
+var CACHE = 'cutprog-v3';
 var FILES = ['./', './index.html', './manifest.webmanifest',
              './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
